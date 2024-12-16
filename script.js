c93c8de732695d071a65385c761bf12d314b2c12
BlairStChair <julia.z15@interia.pl>
2024-12-16 14:25:45 +0100

    Added book constructor and function for adding book objects to array

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//takes user's name to display welcome message
 const hello = document.querySelector(".hello");
 
 var name = prompt("Please enter your name :)");
@@ -5,4 +6,20 @@ var name = prompt("Please enter your name :)");
 console.log(name);
 
 hello.textContent = "Hello " + name + "!";
+
+//book adding logic
+const libraryArray = [];
+
+function Book(author, title, numberOfPages, read) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+}
+
+function addBookToLibrary(author, title, numberOfPages, read) {
+    const book = new Book(author, title, numberOfPages, read);
+    libraryArray.push(Book);
+}
+
  
